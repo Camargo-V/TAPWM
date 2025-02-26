21a32c4dbbfc61d077ac5e1e1213fdfef54fdc8d
Vitor de Camargo <vitor.h.camargo05@gmail.com>
2025-02-26 18:06:43 -0300

    Add files via upload

---
+++ PTesteClasses/PTesteClasses/EMPREGADO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTesteClasses
+{
+    internal class EMPREGADO
+    {
+        private int matrícula;
+        private string nomeEmpregado;
+        private DateTime dataEntradaEmpresa;
+        
+        public int Matrícula
+        {
+            get { return matrícula; }
+            set { matrícula = value; }
+        }
+
+        public string NomeEmpregado
+        {
+            get { return nomeEmpregado; }
+            set {  nomeEmpregado = value; }
+        }
+    }
+}
